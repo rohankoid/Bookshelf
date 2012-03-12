14133e46ee7897ab1b6986d7ca021cc401ca475f
rohankoid <rohankoid@gmail.com>
2012-03-12 11:57:39 +0545

    view helpers and jquery instantiate

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,42 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    /**
+     * Register Namespace
+     */
+    protected function _initRegisterNamespace()
+    {
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        /**
+         * Register ZendX libaray namespace
+         */
+        $autoloader->registerNamespace('ZendX_');
+    }
 
+    /**
+     * Add view Helpers
+     */
+    protected function _initViewHelpers()
+    {
+        $view = new Zend_View();                        
+        /**
+         * add jQuery helper path
+         */
+        $view->addHelperPath("ZendX/JQuery/View/Helper", "ZendX_JQuery_View_Helper");
+
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer();
+        /**
+         * add $view to the view renderer
+         */
+        $viewRenderer->setView($view);
+        /**
+         * add viewRenderer to ActionHelper
+         */
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);                
+
+        return $view;
+
+    }
 
 }
 
